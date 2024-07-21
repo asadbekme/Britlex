70fd25e620427e75574428cdde58c7e43652f372
asadbekme <asadbekme2002@gmail.com>
2024-07-21 22:54:11 +0500

    fix: image src

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -28,28 +28,28 @@ const skillsData = [
       "Improve your English skills and confidence. Live classes and interactive lessons online. 20% extra free for a limited time only",
     secondaryDescription:
       "Learn English online and improve your skills through our high-quality courses and resources – all designed for adult language learners.",
-    imgUrl: "/src/assets/speaking.svg",
+    imgUrl: "/assets/speaking.svg",
     btnTitle: "Learn more",
   },
   {
     title: "Writing",
     primaryDescription:
       "One of the most important and extensive areas of natural science, the science that studies substances, also their composition.",
-    imgUrl: "/src/assets/writing.svg",
+    imgUrl: "/assets/writing.svg",
     btnTitle: "Learn more",
   },
   {
     title: "Reading",
     primaryDescription:
       "Perception and response actions of the user resulting from the use and/or upcoming use of the product, system or service.",
-    imgUrl: "/src/assets/reading.svg",
+    imgUrl: "/assets/reading.svg",
     btnTitle: "Learn more",
   },
   {
     title: "Listening",
     primaryDescription:
       "Here you can find activities to practice your listening skills. Listening will help you to improve your understanding.",
-    imgUrl: "/src/assets/listening.svg",
+    imgUrl: "/assets/listening.svg",
     btnTitle: "Learn more",
   },
 ];
