5b8dcc2559b0e48c8b9e61a9bfc505e4e5c4196e
asadbekme <asadbekme2002@gmail.com>
2024-07-21 22:34:15 +0500

    fix: build error

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,13 @@
 /* eslint-disable no-undef */
 import path from "path";
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
 });
