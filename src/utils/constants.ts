c2a7bdc7888e8a41bcb594c8a9b7440db8948de4
asadbekme <asadbekme2002@gmail.com>
2024-07-22 11:01:41 +0500

    fix: some changes

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,8 @@
+import speakingImg from "@/assets/speaking.svg";
+import writingImg from "@/assets/writing.svg";
+import readingImg from "@/assets/reading.svg";
+import listeningImg from "@/assets/listening.svg";
+
 const navLinks = [
   {
     id: 1,
@@ -28,28 +33,28 @@ const skillsData = [
       "Improve your English skills and confidence. Live classes and interactive lessons online. 20% extra free for a limited time only",
     secondaryDescription:
       "Learn English online and improve your skills through our high-quality courses and resources – all designed for adult language learners.",
-    imgUrl: "/assets/speaking.svg",
+    imgUrl: speakingImg,
     btnTitle: "Learn more",
   },
   {
     title: "Writing",
     primaryDescription:
       "One of the most important and extensive areas of natural science, the science that studies substances, also their composition.",
-    imgUrl: "/assets/writing.svg",
+    imgUrl: writingImg,
     btnTitle: "Learn more",
   },
   {
     title: "Reading",
     primaryDescription:
       "Perception and response actions of the user resulting from the use and/or upcoming use of the product, system or service.",
-    imgUrl: "/assets/reading.svg",
+    imgUrl: readingImg,
     btnTitle: "Learn more",
   },
   {
     title: "Listening",
     primaryDescription:
       "Here you can find activities to practice your listening skills. Listening will help you to improve your understanding.",
-    imgUrl: "/assets/listening.svg",
+    imgUrl: listeningImg,
     btnTitle: "Learn more",
   },
 ];
